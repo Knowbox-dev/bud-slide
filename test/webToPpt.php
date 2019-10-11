8b29af13c1ab4444f0c59f4051060086334de757
zouyikb <zouyi@knowbox.cn>
2019-10-11 12:44:34 +0800

    move

--- test/webToPpt.php
+++ test/webToPpt.php
@@ -5,3 +5,67 @@
  * Date: 2019/10/11
  * Time: 10:57 AM
  */
+set_time_limit(0);
+error_reporting(0);
+ini_set('memory_limit','2G');
+include "../src/vendor/autoload.php";
+
+$pptxFile = __DIR__ . '/xcoutput.pptx';
+$outputFile = __DIR__ . '/xc.json';
+#打开 ppt
+$readers = [
+    'odp' => 'ODPresentation',
+    'ppt' => 'PowerPoint97',
+    'pptx' => 'PowerPoint2007',
+    'phppt' => 'Serialized',
+];
+$data = file_get_contents($outputFile);
+print_r($data);
+exit;
+$oReader = new \PhpOffice\PhpPresentation\PhpPresentation();
+$ppt = $oReader->load($pptxFile);
+#文档属性
+$dp = $ppt->getDocumentProperties();
+$dp->getTitle();
+$dp->getCreator();
+$dp->getLastModifiedBy();
+
+#ppt 属性
+$pp = $ppt->getPresentationProperties();
+$pp->getLastView();
+$pp->getZoom();
+$pp->isMarkedAsFinal();
+
+#布局
+$layout = $ppt->getLayout();
+$cx = $layout->getCX();
+$cy = $layout->getCY();
+//print_r([$cx, $cy]);
+
+#获取 ppt 页数
+$slideCount = $ppt->getSlideCount();
+
+$slides = $ppt->getAllSlides();
+foreach ($slides as $slide)
+{
+    $slide->getBackground();
+    $slide->getAnimations();
+    $slide->getName();
+    $slide->getNote();
+    $sc = $slide->getShapeCollection();
+    foreach ($sc->getIterator() as $sci)
+    {
+    }
+}
+#获取当前页的元素
+
+#获取元素的样式
+
+
+
+#打开 ppt
+#获取 ppt 页数
+
+#获取当前页的元素
+
+#获取元素的样式
